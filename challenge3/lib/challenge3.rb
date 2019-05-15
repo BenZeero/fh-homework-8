f2a343c48eed2f1c56896b9451bb9b8f6c7d1d81
Firstname Lastname <e-mail address>
2019-05-15 16:52:36 -0700

    update

--- challenge3/lib/challenge3.rb
+++ challenge3/lib/challenge3.rb
@@ -2,10 +2,28 @@ class Challenge3
   attr_reader :input_string
 
   def initialize(input_string)
-    # write your code here    
+    # write your code here
+    @input_string = input_string    
   end
 
   def rewrite
     # write your code here
+    counter=0
+    verify_letter= nil
+    sum_letter=Array.new
+    @input_string.split("").to_a.each do |letter|
+      if verify_letter == nil
+        verify_letter = letter
+        counter = counter + 1
+      elsif verify_letter == letter
+        counter = counter + 1
+      elsif verify_letter != letter
+        sum_letter << "#{counter}#{verify_letter}"
+        verify_letter = letter
+        counter = 1
+      end
+    end
+    sum_letter << "#{counter}#{verify_letter}"
+    sum_letter.join()
   end
 end
--- challenge4/lib/challenge4.rb
+++ challenge4/lib/challenge4.rb
@@ -1,4 +1,5 @@
 class Challenge4
+  attr_reader :number, :num
   def self.factorial(number)
     _factorial(number, 1)
   end
@@ -7,5 +8,18 @@ class Challenge4
 
   def self._factorial(number, value)
     # write your solution here
+    result = 1
+    number.to_s.split("_").each do |num| 
+      num = num.to_i
+      if num < 0
+          value
+      elsif num == 0
+          value
+      else
+        result = result * num
+        num -= 1
+        result
+      end
+    end
   end
 end
